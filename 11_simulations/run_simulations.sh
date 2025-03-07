7dc2b098b91b474ce1ccad415a372bbd4cfd7877
Robert Wan <37159376+rw417@users.noreply.github.com>
2025-03-07 13:02:35 -0500

    small changes

--- 11_simulations/run_simulations.sh
+++ 11_simulations/run_simulations.sh
@@ -1,15 +1,14 @@
-# !/bin/bash
-# SBATCH --array=1-4%4
-# SBATCH --job-name=run_sim_job
-# SBATCH --partition=wrobel
-# SBATCH --output=run_sim.out
-# SBATCH --error=run_sim.err
+#!/bin/bash
+#SBATCH --array=1-4%4
+#SBATCH --job-name=run_sim_job
+#SBATCH --partition=wrobel
+#SBATCH --output=run_sim.out
+#SBATCH --error=run_sim.err
 
 module purge
 module load R
 
 # Rscript to run an r script
 # This stores which job is running (1, 2, 3, etc)
-
 JOBID=$SLURM_ARRAY_TASK_ID
 Rscript run_simulations.R $JOBID
